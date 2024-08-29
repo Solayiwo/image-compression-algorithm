933607bfc447b83183132e90eab28f8dd4c0a307
Olayiwola Sulaimon <osulaimon19@yahoo.com>
2024-08-29 04:27:44 +0100

    Serializer

---
+++ backend/compression_app/serializers.py
@@ -0,0 +1,33 @@
+from rest_framework import serializers
+from django.contrib.auth.hashers import make_password
+from .models import CustomUser, GuestUser, CompressionTask
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = CustomUser
+        fields = '__all__'
+        extra_kwargs = {'password': {'write_only': True}}
+
+        def create(self, validated_data):
+            validated_data['password'] = make_password(validated_data['password'])
+            user = CustomUser.objects.create_user(**validated_data)
+            return user
+        
+        
+class GuestUserSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = GuestUser
+        field = '__all__'
+    
+    def create(self, validated_data):
+            guest_user = GuestUser.objects.create_user(**validated_data)
+            return guest_user
+
+
+class ImageCompressionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CompressionTask
+        field = '__all__'
+        ('id', 'user', 'guest_user', 'original_image', 'compressed_image', 'compression_type', 'uploaded_at')
