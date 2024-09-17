340a147897a700d7ce1301ad2fa5b5e07869b50d
Olayiwola Sulaimon <osulaimon19@yahoo.com>
2024-09-17 10:20:11 +0100

    Test case modification

--- backend/image_compression/settings.py
+++ backend/image_compression/settings.py
@@ -125,10 +125,10 @@ AUTH_PASSWORD_VALIDATORS = [
     'django.contrib.auth.hashers.BCryptSHA256PasswordHasher'
 ]"""
 
-PASSWORD_HASHERS = [
+"""PASSWORD_HASHERS = [
     'django.contrib.auth.hashers.PBKDF2PasswordHasher',
     'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
-]
+]"""
 
 
 AUTHENTICATION_BACKENDS = [
