4fcb64591cee921d6c892e43e90cd66690e04d3b
Olayiwola Sulaimon <osulaimon19@yahoo.com>
2024-09-17 10:12:38 +0100

    Fix: ImageCompressView

--- backend/compression_app/views.py
+++ backend/compression_app/views.py
@@ -4,10 +4,14 @@ from rest_framework import generics, permissions
 #rom django.contrib.auth import get_user_model
 from django.contrib.auth.hashers import check_password
 from django.contrib.auth import get_user_model, login
+from rest_framework import status
+from django.core.files.uploadedfile import InMemoryUploadedFile
+from io import BytesIO
+import sys
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import CustomUser, GuestUser, CompressionTask
-from .serializers import UserSerializer, GuestUserSerializers, ImageCompressionSerializer
+from .serializers import UserSerializer, ImageCompressionSerializer
 from .compression import compress_image
 from django.http import FileResponse
 from .compression_report import generate_pdf_report
@@ -73,25 +77,57 @@ class LoginView(APIView):
             login(request, user)
             return Response({'message': 'Login successful'})
         return Response({'message': 'Invalid credentials'}, status=400)"""
-    
-class ImageUploadView(generics.CreateAPIView):
-    queryset = CompressionTask.objects.all()
-    serializer_class = ImageCompressionSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
-    def peform_create(self, serializer):
-        # Add compression logic here
-        uploaded_image = self.request.FILES.get('image')
-        compressed_image = compress_image(uploaded_image)
-        serializer.save(user=self.request.user if self.request.user.is_authenticated else None, compressed_image=compressed_image)
+class ImageCompressView(APIView):
+    def post(self, request, *args, **kwargs):
+        # Check if the user is authenticated
+        if request.user.is_authenticated:
+            user = request.user
+            is_guest = False
+        else:
+            # If not authenticated, handle as guest user
+            if not request.session.session_key:
+                request.session.create()
+            session_id = request.session.session_key
+            user, created = GuestUser.objects.get_or_create(session_id=session_id)
+            is_guest = True
 
+        serializer = ImageCompressionSerializer(data=request.data)
+        if serializer.is_valid():
+            image = serializer.validated_data['compressed_image']
+            compression_type = serializer.validated_data['compression_type']
+
+            # Compress the image
+            compressed_image_path = compress_image(image, compression_type)
+
+             # Create an InMemoryUploadedFile from the BytesIO object
+            compressed_image_file = InMemoryUploadedFile(
+                file=compressed_image_path,
+                field_name='compressed_image',
+                name=image.name,
+                content_type=image.content_type,
+                size=sys.getsizeof(compressed_image_path),
+                charset=None
+            )
+
+            # Save the image to the database associated with the user (guest or authenticated)
+            CompressionTask.objects.create(
+                user=user if not is_guest else None,  # If logged-in user, store user
+                guest_user=user if is_guest else None,  # If guest user, store guest_user
+                compressed_image=compressed_image_file,
+                compression_type=compression_type
+            )
+
+            return Response({'image_path': compressed_image_path}, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class PDFReportView(APIView):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def get(self, request, *args, **kwargs):
         user = request.user
-        compressed_images = CompressionTask.objects.filter(user=user)
+        compressed_images = CompressionTask.objects.get(user=user)
         pdf_buffer = generate_pdf_report(compressed_images)
         return FileResponse(pdf_buffer, as_attachment=True, filename='compression_report.pdf')
     
